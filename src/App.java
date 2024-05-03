ca4a15341fe77849f09eb14de5ef5ef971e70124
Isra-DVD <usr3849@salesianos-lacuesta.net>
2024-05-03 15:36:16 +0100

    feat(app): Implementada clase ejecutable con sus funcionalidades

--- src/App.java
+++ src/App.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+import net.salesianos.menu.Menu;
+import net.salesianos.trabajadores.Cajero;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner escaner = new Scanner(System.in);
+        boolean seguirPreguntando = true;
+        Cajero caja = new Cajero(1);
+
+        do {
+
+            System.out.println(Menu.mostrar());
+            String opcionUsuario = escaner.nextLine();
+
+            switch (opcionUsuario) {
+                case "1":
+                    caja.abrirCaja();
+                    break;
+                case "2":
+                    caja.agregarClienteACola();
+                    break;
+                case "3":
+                    caja.atenderCliente();
+                    break;
+                case "4":
+                    System.out.println(caja.verClientesPendientes());
+                    break;
+
+                case "5":
+                    seguirPreguntando = caja.cerrarSupermercado(caja);
+                    break;
+
+                default:
+                    System.out.println("Ha introducido un valor erroneo, vuelva a intentarlo\n");
+                    break;
+            }
+
+        } while (seguirPreguntando);
+
+        escaner.close();
     }
 }
